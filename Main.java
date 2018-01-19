5691ab4537e6f3b1a7e581651e0b29c00010114b
Erbenner <909382526@qq.com>
2018-01-19 22:16:26 +0800

    Main.java
    程序从这里启动

---
+++ Main.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+/**
+ * @author Erbenner
+ *
+ */
+package com.main;
+
+import com.ui.AppBody;
+
+public class Main{
+	public static void main(String [] args){
+		new AppBody();
+	}
+}
